3e6f8097727e07c510f9441036bdfd11ff2ceeb5
BOC1096 <linh000boc@gmail.com>
2020-08-04 00:11:46 +0700

    update

--- CaseStudy-Web/furama/src/main/java/com/codegym/furama/FuramaApplication.java
+++ CaseStudy-Web/furama/src/main/java/com/codegym/furama/FuramaApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Module2TienNvtWbeApplication {
+public class FuramaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Module2TienNvtWbeApplication.class, args);
+        SpringApplication.run(FuramaApplication.class, args);
     }
 }
